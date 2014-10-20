16fd2c877637b6c9c5d194a3608221f747a68a30
cntopcode <kakukaitu@watersdeMac-mini.local>
2014-10-20 23:50:43 +0800

    10-20

--- gen/com/water/imemo/R.java
+++ gen/com/water/imemo/R.java
@@ -25,12 +25,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080005;
+        public static final int editorSaveButton=0x7f080001;
         public static final int memoEditor=0x7f080000;
+        public static final int memoList=0x7f080002;
+        public static final int memoSummary=0x7f080003;
+        public static final int memoTime=0x7f080004;
     }
     public static final class layout {
         public static final int activity_index=0x7f030000;
         public static final int editor=0x7f030001;
+        public static final int memolist=0x7f030002;
+        public static final int memolistitem=0x7f030003;
     }
     public static final class menu {
         public static final int index=0x7f070000;
@@ -38,7 +44,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int dummy=0x7f050005;
         public static final int editorDefaultHint=0x7f050003;
+        public static final int editorSaveButtonText=0x7f050004;
         public static final int hello_world=0x7f050001;
     }
     public static final class style {
